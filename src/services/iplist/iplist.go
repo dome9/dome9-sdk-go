9fc14910b5a7ea954a861171b337157b752bbce4
Tal Sheldon <tal.sheldon@dome9.com>
2019-09-05 15:54:41 +0300

    Fix IpList (#9)
    - Add nil as option argument
    - Change ID argument to int64

--- src/services/iplist/iplist.go
+++ src/services/iplist/iplist.go
@@ -29,10 +29,10 @@ func New(c *dome9.Config) *Service {
 	}
 }
 
-func (ipLists *Service) Get(ipListId int) (*IpList, *http.Response, error) {
+func (ipLists *Service) Get(ipListId int64) (*IpList, *http.Response, error) {
 	v := new(IpList)
 	path := fmt.Sprintf("iplist/%d", ipListId)
-	resp, err := ipLists.client.NewRequestDo("GET", path, nil, v)
+	resp, err := ipLists.client.NewRequestDo("GET", path, nil, nil, v)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -42,7 +42,7 @@ func (ipLists *Service) Get(ipListId int) (*IpList, *http.Response, error) {
 func (ipLists *Service) GetAll() (*[]IpList, *http.Response, error) {
 	v := new([]IpList)
 	path := "iplist/"
-	resp, err := ipLists.client.NewRequestDo("GET", path, nil, v)
+	resp, err := ipLists.client.NewRequestDo("GET", path, nil, nil, v)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -51,27 +51,27 @@ func (ipLists *Service) GetAll() (*[]IpList, *http.Response, error) {
 
 func (ipLists *Service) Create(ipList *IpList) (*IpList, *http.Response, error) {
 	v := new(IpList)
-	resp, err := ipLists.client.NewRequestDo("POST", "iplist/", ipList, &v)
+	resp, err := ipLists.client.NewRequestDo("POST", "iplist/", nil, ipList, &v)
 	if err != nil {
 		return nil, nil, err
 	}
 	return v, resp, nil
 }
 
-func (ipLists *Service) Update(ipListId string, ipList *IpList) (*http.Response, error) {
-	path := fmt.Sprintf("iplist/%s", ipListId)
+func (ipLists *Service) Update(ipListId int64, ipList *IpList) (*http.Response, error) {
+	path := fmt.Sprintf("iplist/%d", ipListId)
 	// v is nil because updating iplist returns nothing (204)
-	resp, err := ipLists.client.NewRequestDo("PUT", path, ipList, nil)
+	resp, err := ipLists.client.NewRequestDo("PUT", path, ipList, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 	return resp, err
 }
 
-func (ipLists *Service) Delete(ipListId string) (*http.Response, error) {
-	path := fmt.Sprintf("iplist/%s", ipListId)
+func (ipLists *Service) Delete(ipListId int64) (*http.Response, error) {
+	path := fmt.Sprintf("iplist/%d", ipListId)
 	// v is nil because deleting iplist returns nothing (204)
-	resp, err := ipLists.client.NewRequestDo("DELETE", path, nil, nil)
+	resp, err := ipLists.client.NewRequestDo("DELETE", path, nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
