54396c98e3d3731a987af304d1276e9ea46e7a0d
erez-dome9 <erez@dome9.com>
2019-09-15 14:14:10 +0300

    Cloudaccount AWS structs refactor (#23)

--- services/cloudaccount/cloud_account.go
+++ services/cloudaccount/cloud_account.go
@@ -14,125 +14,105 @@ const (
 	D9GCPResourceName   = "GoogleCloudAccount/"
 )
 
-type AwsCredentials struct {
-	// ApiKey and Arn cannot exist together because Dome9 allows only one. To avoid passing empty strings (""),
-	// we use pointers and the request is being sent with <nil>
-	ApiKey     *string `json:"apikey"`
-	Arn        *string `json:"arn"`
-	Secret     string  `json:"secret"`
-	IamUser    string  `json:"iamUser"`
-	Type       string  `json:"type"`
-	IsReadOnly bool    `json:"isReadOnly"`
+// AWSCloudAccountRequest and AWSCloudAccountResponse refer to API type: CloudAccount
+type AWSCloudAccountRequest struct {
+	Vendor                string    `json:"vendor"`
+	Name                  string    `json:"name"`
+	ExternalAccountNumber string    `json:"externalAccountNumber"`
+	Error                 *string   `json:"error"`
+	IsFetchingSuspended   bool      `json:"isFetchingSuspended"`
+	CreationDate          time.Time `json:"creationDate"`
+	Credentials           struct {
+		ApiKey     *string `json:"apikey"`
+		Arn        *string `json:"arn"`
+		Secret     string  `json:"secret"`
+		IamUser    string  `json:"iamUser"`
+		Type       string  `json:"type"`
+		IsReadOnly bool    `json:"isReadOnly"`
+	} `json:"credentials"`
+	FullProtection         bool   `json:"fullProtection"`
+	AllowReadOnly          bool   `json:"allowReadOnly"`
+	OrganizationalUnitID   string `json:"organizationalUnitId"`
+	OrganizationalUnitPath string `json:"organizationalUnitPath"`
+	OrganizationalUnitName string `json:"organizationalUnitName"`
+	LambdaScanner          bool   `json:"lambdaScanner"`
 }
 
-type AwsRegion struct {
-	Region           string `json:"awsRegion"`
-	Name             string `json:"name"`
-	Hidden           bool   `json:"hidden"`
-	NewGroupBehavior string `json:"newGroupBehavior"`
+type AWSCloudAccountResponse struct {
+	ID                    string    `json:"id"`
+	Vendor                string    `json:"vendor"`
+	Name                  string    `json:"name"`
+	ExternalAccountNumber string    `json:"externalAccountNumber"`
+	Error                 *string   `json:"error"`
+	IsFetchingSuspended   bool      `json:"isFetchingSuspended"`
+	CreationDate          time.Time `json:"creationDate"`
+	Credentials           struct {
+		ApiKey     *string `json:"apikey"`
+		Arn        *string `json:"arn"`
+		Secret     *string `json:"secret"`
+		IamUser    *string `json:"iamUser"`
+		Type       string  `json:"type"`
+		IsReadOnly bool    `json:"isReadOnly"`
+	} `json:"credentials"`
+	IamSafe *struct {
+		AwsGroupArn         string `json:"awsGroupArn"`
+		AwsPolicyArn        string `json:"awsPolicyArn"`
+		Mode                string `json:"mode"`
+		State               string `json:"state"`
+		ExcludedIamEntities struct {
+			RolesArns []string `json:"rolesArns"`
+			UsersArns []string `json:"usersArns"`
+		} `json:"excludedIamEntities"`
+		RestrictedIamEntities struct {
+			RolesArns []string `json:"rolesArns"`
+			UsersArns []string `json:"usersArns"`
+		} `json:"restrictedIamEntities"`
+	} `json:"iamSafe"`
+	NetSec struct {
+		Regions []struct {
+			Region           string `json:"awsRegion"`
+			Name             string `json:"name"`
+			Hidden           bool   `json:"hidden"`
+			NewGroupBehavior string `json:"newGroupBehavior"`
+		} `json:"regions"`
+	} `json:"netSec"`
+	Magellan               bool    `json:"magellan"`
+	FullProtection         bool    `json:"fullProtection"`
+	AllowReadOnly          bool    `json:"allowReadOnly"`
+	OrganizationalUnitID   *string `json:"organizationalUnitId"`
+	OrganizationalUnitPath string  `json:"organizationalUnitPath"`
+	OrganizationalUnitName string  `json:"organizationalUnitName"`
+	LambdaScanner          bool    `json:"lambdaScanner"`
 }
 
-type AwsNetSec struct {
-	Regions []AwsRegion `json:"regions"`
+// AzureCloudAccountRequest and AzureCloudAccountResponse refer to API type: AzureCloudAccount
+type AzureCloudAccountRequest struct {
+	Name           string `json:"name"`
+	SubscriptionID string `json:"subscriptionId"`
+	TenantID       string `json:"tenantId"`
+	Credentials    struct {
+		ClientID       string `json:"clientId"`
+		ClientPassword string `json:"clientPassword"`
+	} `json:"credentials"`
+	OperationMode string `json:"operationMode"`
 }
 
-// refer to API type: CloudAccountIamSafe
-type AwsIAMSafe struct {
-	AwsGroupArn           string         `json:"awsGroupArn"`
-	AwsPolicyArn          string         `json:"awsPolicyArn"`
-	Mode                  string         `json:"mode"`
-	State                 string         `json:"state"`
-	ExcludedIamEntities   AWSIamEntities `json:"excludedIamEntities"`
-	RestrictedIamEntities AWSIamEntities `json:"restrictedIamEntities"`
-}
-
-type AWSIamEntities struct {
-	RolesArns []string `json:"rolesArns"`
-	UsersArns []string `json:"usersArns"`
-}
-
-// refer to API type: CloudAccount
-type AwsGetCloudAccountResponse struct {
-	ID                     string         `json:"id"`
-	Vendor                 string         `json:"vendor"`
-	Name                   string         `json:"name"`
-	ExternalAccountNumber  string         `json:"externalAccountNumber"`
-	Error                  string         `json:"error"`
-	IsFetchingSuspended    bool           `json:"isFetchingSuspended"`
-	CreationDate           time.Time      `json:"creationDate"`
-	Credentials            AwsCredentials `json:"credentials"`
-	IamSafe                AwsIAMSafe     `json:"iamSafe"`
-	NetSec                 AwsNetSec      `json:"awsNetSec"`
-	Magellan               bool           `json:"magellan"`
-	FullProtection         bool           `json:"fullProtection"`
-	AllowReadOnly          bool           `json:"allowReadOnly"`
-	OrganizationalUnitID   string         `json:"organizationalUnitId"`
-	OrganizationalUnitPath string         `json:"organizationalUnitPath"`
-	OrganizationalUnitName string         `json:"organizationalUnitName"`
-	LambdaScanner          bool           `json:"lambdaScanner"`
-}
-
-// refer to API type: CloudAccount
-type AwsCreateRequest struct {
-	Vendor                 string         `json:"vendor"`
-	Name                   string         `json:"name"`
-	ExternalAccountNumber  string         `json:"externalAccountNumber"`
-	Error                  *string        `json:"error"`
-	IsFetchingSuspended    bool           `json:"isFetchingSuspended"`
-	CreationDate           time.Time      `json:"creationDate"`
-	Credentials            AwsCredentials `json:"credentials"`
-	FullProtection         bool           `json:"fullProtection"`
-	AllowReadOnly          bool           `json:"allowReadOnly"`
-	OrganizationalUnitID   string         `json:"organizationalUnitId"`
-	OrganizationalUnitPath string         `json:"organizationalUnitPath"`
-	OrganizationalUnitName string         `json:"organizationalUnitName"`
-	LambdaScanner          bool           `json:"lambdaScanner"`
-}
-
-type AzureCredentials struct {
-	ClientID       string `json:"clientId"`
-	ClientPassword string `json:"clientPassword"`
-}
-
-// refer to API type: AzureCloudAccount
-type AzureGetCloudAccountResponse struct {
-	ID                     string           `json:"id"`
-	Name                   string           `json:"name"`
-	SubscriptionID         string           `json:"subscriptionId"`
-	TenantID               string           `json:"tenantId"`
-	Credentials            AzureCredentials `json:"credentials"`
-	OperationMode          string           `json:"operationMode"`
-	Error                  string           `json:"error"`
-	CreationDate           time.Time        `json:"creationDate"`
-	OrganizationalUnitID   string           `json:"organizationalUnitId"`
-	OrganizationalUnitPath string           `json:"organizationalUnitPath"`
-	OrganizationalUnitName string           `json:"organizationalUnitName"`
-	Vendor                 string           `json:"vendor"`
-}
-
-// refer to API type: AzureCloudAccount
-type AzureCreateRequest struct {
-	Name           string           `json:"name"`
-	SubscriptionID string           `json:"subscriptionId"`
-	TenantID       string           `json:"tenantId"`
-	Credentials    AzureCredentials `json:"credentials"`
-	OperationMode  string           `json:"operationMode"`
-}
-
-// refer to API type: GoogleCloudAccountGet
-type GCPCloudAccountResponse struct {
-	ID                     string    `json:"id"`
-	Name                   string    `json:"name"`
-	ProjectID              string    `json:"projectId"`
+type AzureCloudAccountResponse struct {
+	ID             string `json:"id"`
+	Name           string `json:"name"`
+	SubscriptionID string `json:"subscriptionId"`
+	TenantID       string `json:"tenantId"`
+	Credentials    struct {
+		ClientID       string  `json:"clientId"`
+		ClientPassword *string `json:"clientPassword"`
+	} `json:"credentials"`
+	OperationMode          string    `json:"operationMode"`
+	Error                  *string   `json:"error"`
 	CreationDate           time.Time `json:"creationDate"`
 	OrganizationalUnitID   *string   `json:"organizationalUnitId"`
 	OrganizationalUnitPath string    `json:"organizationalUnitPath"`
 	OrganizationalUnitName string    `json:"organizationalUnitName"`
-	Gsuite                 *struct {
-		GsuiteUser string `json:"gsuiteUser"`
-		DomainName string `json:"domainName"`
-	} `json:"gsuite"`
-	Vendor string `json:"vendor"`
+	Vendor                 string    `json:"vendor"`
 }
 
 // refer to API type: GoogleCloudAccountPost
@@ -154,6 +134,22 @@ type GCPCloudAccountRequest struct {
 	DomainName string `json:"domainName"`
 }
 
+// refer to API type: GoogleCloudAccountGet
+type GCPCloudAccountResponse struct {
+	ID                     string    `json:"id"`
+	Name                   string    `json:"name"`
+	ProjectID              string    `json:"projectId"`
+	CreationDate           time.Time `json:"creationDate"`
+	OrganizationalUnitID   *string   `json:"organizationalUnitId"`
+	OrganizationalUnitPath string    `json:"organizationalUnitPath"`
+	OrganizationalUnitName string    `json:"organizationalUnitName"`
+	Gsuite                 *struct {
+		GsuiteUser string `json:"gsuiteUser"`
+		DomainName string `json:"domainName"`
+	} `json:"gsuite"`
+	Vendor string `json:"vendor"`
+}
+
 type QueryParameters struct {
 	ID string
 }
@@ -166,8 +162,8 @@ func New(c *dome9.Config) *Service {
 	return &Service{client: client.NewClient(c)}
 }
 
-func (service *Service) GetCloudAccountAWS(options interface{}) (*AwsGetCloudAccountResponse, *http.Response, error) {
-	v := new(AwsGetCloudAccountResponse)
+func (service *Service) GetCloudAccountAWS(options interface{}) (*AWSCloudAccountResponse, *http.Response, error) {
+	v := new(AWSCloudAccountResponse)
 	resp, err := service.client.NewRequestDo("GET", D9AwsResourceName, options, nil, v)
 	if err != nil {
 		return nil, nil, err
@@ -176,8 +172,8 @@ func (service *Service) GetCloudAccountAWS(options interface{}) (*AwsGetCloudAcc
 	return v, resp, nil
 }
 
-func (service *Service) CreateCloudAccountAWS(body interface{}) (*AwsGetCloudAccountResponse, *http.Response, error) {
-	v := new(AwsGetCloudAccountResponse)
+func (service *Service) CreateCloudAccountAWS(body interface{}) (*AWSCloudAccountResponse, *http.Response, error) {
+	v := new(AWSCloudAccountResponse)
 	resp, err := service.client.NewRequestDo("POST", D9AwsResourceName, nil, body, v)
 	if err != nil {
 		return nil, nil, err
@@ -206,8 +202,8 @@ func (service *Service) CreateCloudAccountGCP(body interface{}) (*GCPCloudAccoun
 	return v, resp, nil
 }
 
-func (service *Service) GetCloudAccountAzure(options interface{}) (*AzureGetCloudAccountResponse, *http.Response, error) {
-	v := new(AzureGetCloudAccountResponse)
+func (service *Service) GetCloudAccountAzure(options interface{}) (*AzureCloudAccountResponse, *http.Response, error) {
+	v := new(AzureCloudAccountResponse)
 	resp, err := service.client.NewRequestDo("GET", D9AzureResourceName, options, nil, v)
 	if err != nil {
 		return nil, nil, err
@@ -216,8 +212,8 @@ func (service *Service) GetCloudAccountAzure(options interface{}) (*AzureGetClou
 	return v, resp, nil
 }
 
-func (service *Service) CreateCloudAccountAzure(body interface{}) (*AzureGetCloudAccountResponse, *http.Response, error) {
-	v := new(AzureGetCloudAccountResponse)
+func (service *Service) CreateCloudAccountAzure(body interface{}) (*AzureCloudAccountResponse, *http.Response, error) {
+	v := new(AzureCloudAccountResponse)
 	resp, err := service.client.NewRequestDo("POST", D9AzureResourceName, nil, body, v)
 	if err != nil {
 		return nil, nil, err
