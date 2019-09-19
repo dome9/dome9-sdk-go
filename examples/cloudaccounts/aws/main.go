8085a21eb0c0d8057f4c03fdb32836f651f633f7
erez-dome9 <erez@dome9.com>
2019-09-19 14:41:20 +0300

    Usage examples (#33)
    - Cloudaccount (AWS, Azure, GCP) usage examples
    - Compliance (policy, notification) usage examples
    - IPList usage example

---
+++ examples/cloudaccounts/aws/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dome9/dome9-sdk-go/dome9"
+	"github.com/Dome9/dome9-sdk-go/services/cloudaccounts/aws"
+)
+
+func main() {
+	// Pass accessID, secretKey, rawUrl, or set environment variables
+	config, _ := dome9.NewConfig("", "", "")
+	srv := aws.New(config)
+	var req aws.CloudAccountRequest
+
+	req.Name = "test AWS cloud account"
+	req.FullProtection = false
+	req.AllowReadOnly = false
+	req.Vendor = "aws"
+	req.Credentials.Arn = "ARN"
+	req.Credentials.Secret = "SECRET"
+	req.Credentials.Type = "RoleBased"
+
+	v, _, err := srv.Create(req)
+	resp, _, _ := srv.GetAll()
+	fmt.Printf("Create response type: %T\n Content %+v\n", v, v)
+	fmt.Printf("Get response type: %T\n Content: %+v\n", resp, resp)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
---
+++ examples/iplist/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dome9/dome9-sdk-go/dome9"
+	"github.com/Dome9/dome9-sdk-go/services/iplist"
+)
+
+func main() {
+	// Pass accessID, secretKey, rawUrl, or set environment variables
+	config, _ := dome9.NewConfig("", "", "")
+	srv := iplist.New(config)
+	var req iplist.IpList
+	// Set IP List parameters
+	req.Name = "test IP list"
+	req.Description = "test description"
+	item := struct {
+		Ip string
+		Comment string
+	}{
+		Ip: "6.6.6.6",
+		Comment: "This is comment",
+	}
+	req.Items = append(req.Items, item)
+
+	v, _, err := srv.Create(&req)
+	resp, _, _ := srv.GetAll()
+	fmt.Printf("Create response type: %T\n Content %+v", v, v)
+	fmt.Printf("Get response type: %T\n Content: %+v", resp, resp)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
