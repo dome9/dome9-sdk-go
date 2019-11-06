9461c186cfa3511ba70e8ccfb2a2a13f9925dd9f
Ayal Ganem <ayalg@checkpoint.com>
2019-11-06 13:51:13 +0200

    Service CloudSecurityGroup AWS (#81)
    - AWS security group service and CRUD
    - AWS security group example

---
+++ examples/cloud_security_group/security_group_aws/README.md
@@ -0,0 +1,131 @@
+```go
+package main
+
+import (
+	"fmt"
+
+	"github.com/dome9/dome9-sdk-go/dome9"
+	"github.com/dome9/dome9-sdk-go/services/cloudsecuritygroup/securitygroupaws"
+)
+
+func main() {
+	config, _ := dome9.NewConfig("", "", "")
+	srv := securitygroupaws.New(config)
+
+	scope := securitygroupaws.Scope{
+		Type: "CIDR",
+		Data: map[string]string{
+			"cidr": "0.0.0.0/0",
+		    "note": "Allow All Traffic",
+		},
+	}
+
+	inbound := securitygroupaws.BoundServicesRequest{
+		Name:         "NAME",
+		Description:  "DESCRIPTION",
+		ProtocolType: "TCP",
+		Port:         "12",
+		OpenForAll:   true,
+		Scope: []securitygroupaws.Scope{
+			scope,
+		},
+	}
+
+	outbound := securitygroupaws.BoundServicesRequest{
+		Name:         "Name",
+		Description:  "DESCRIPTION",
+		ProtocolType: "ALL",
+		Port:         "",
+		OpenForAll:   true,
+	}
+
+	req := securitygroupaws.CloudSecurityGroupRequest{
+		IsProtected:       true,
+		SecurityGroupName: "Name",
+		Description:       "DESCRIPTION",
+		RegionId:          "us_west_1",
+		CloudAccountId:    "00000000-0000-0000-0000-000000000000",
+		Services: securitygroupaws.ServicesRequest{
+			Inbound: []securitygroupaws.BoundServicesRequest{
+				inbound,
+			},
+			Outbound: []securitygroupaws.BoundServicesRequest{
+				outbound,
+			},
+		},
+		Tags: map[string]string{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	// create AWS SG
+	v, _, err := srv.Create(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Create response type: %T\n Content %+v", *v, *v)
+
+	// get AWS SG
+	awsSecurityGroup, _, err := srv.GetSecurityGroup("sg-0edd194c096c47afc")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Get specific security group response type: %T\n Content: %+v", *awsSecurityGroup, *awsSecurityGroup)
+
+	// get all AWS SG
+	allAWSSecurityGroups, _, err := srv.GetAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("GetAll response type: %T\n Content: %+v", *allAWSSecurityGroups, *allAWSSecurityGroups)
+
+	// get a all AWS SG in region
+	someAWSSecurityGroup, _, err := srv.GetAllInRegion("00000000-0000-0000-0000-000000000000", "us_west_1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Get response type: %T\n Content: %+v", *someAWSSecurityGroup, *someAWSSecurityGroup)
+
+    // update AWS SG
+	securityGroupResponse, _, err := srv.Update("sg-00000000000000000", securitygroupaws.CloudSecurityGroupRequest{
+		IsProtected:       true,
+		SecurityGroupName: "NAME",
+		Description:       "DESCRIPTION",
+		RegionId:          "us_west_1",
+		CloudAccountId:    "00000000-0000-0000-0000-000000000000",
+		Services: securitygroupaws.ServicesRequest{
+			Inbound: []securitygroupaws.BoundServicesRequest{
+			},
+			Outbound: []securitygroupaws.BoundServicesRequest{
+			},
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Update AWS security group: %T\n Content: %+v\n", *securityGroupResponse, *securityGroupResponse)
+
+	// update protection mode
+	securityGroupResponse, _, err = srv.UpdateProtectionMode("sg-00000000000000000", "FullManage")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Update protection mode mode: %T\n Content: %+v\n", *securityGroupResponse, *securityGroupResponse)
+
+	// create and attach or update bound service
+	securityGroupResponse, _, err = srv.HandelBoundServices("sg-00000000000000000", "Inbound", inbound)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Update bound services: %T\n Content: %+v\n", *securityGroupResponse, *securityGroupResponse)
+
+	// delete AWS SG
+	_, err = srv.Delete("sg-00000000000000000")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("AWS Security Group deleted")
+}
+
+```
---
+++ services/cloudsecuritygroup/securitygroupaws/securitygroupaws.go
@@ -0,0 +1,195 @@
+package securitygroupaws
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	awsSgResourcePath           = "CloudSecurityGroup"
+	awsSgResourceProtectionMode = "protection-mode"
+	awsSgResourceServices       = "services"
+)
+
+// There is a bug when we pass nil inbound or outbound, ticket link: https://dome9-security.atlassian.net/browse/DOME-12727
+type CloudSecurityGroupRequest struct {
+	IsProtected       bool              `json:"isProtected"`
+	SecurityGroupName string            `json:"securityGroupName"`
+	Description       string            `json:"description"`
+	RegionId          string            `json:"regionId"`
+	CloudAccountId    string            `json:"cloudAccountId"`
+	Services          ServicesRequest   `json:"services"`
+	Tags              map[string]string `json:"tags,omitempty"`
+}
+
+type CloudSecurityGroupResponse struct {
+	SecurityGroupID   int               `json:"securityGroupId"`
+	ExternalID        string            `json:"externalId"`
+	IsProtected       bool              `json:"isProtected"`
+	SecurityGroupName string            `json:"securityGroupName"`
+	Description       string            `json:"description"`
+	VpcID             string            `json:"vpcId"`
+	VpcName           string            `json:"vpcName,omitempty"`
+	RegionName        string            `json:"regionId"`
+	CloudAccountId    string            `json:"cloudAccountId"`
+	CloudAccountName  string            `json:"cloudAccountName"`
+	Services          ServicesResponse  `json:"services"`
+	Tags              map[string]string `json:"tags"`
+}
+
+type ServicesRequest struct {
+	Inbound  []BoundServicesRequest `json:"inbound"`
+	Outbound []BoundServicesRequest `json:"outbound"`
+}
+
+type ServicesResponse struct {
+	Inbound  []BoundServicesResponse `json:"inbound"`
+	Outbound []BoundServicesResponse `json:"outbound"`
+}
+
+type BoundServicesRequest struct {
+	Name         string  `json:"name"`
+	Description  string  `json:"description"`
+	ProtocolType string  `json:"protocolType"`
+	Port         string  `json:"port"`
+	OpenForAll   bool    `json:"openForAll"`
+	Scope        []Scope `json:"scope"`
+}
+
+type BoundServicesResponse struct {
+	Id           string  `json:"id"`
+	Name         string  `json:"name"`
+	Description  string  `json:"description"`
+	ProtocolType string  `json:"protocolType"`
+	Port         string  `json:"port"`
+	OpenForAll   bool    `json:"openForAll"`
+	Scope        []Scope `json:"scope"`
+	Inbound      bool    `json:"inbound"`
+	ICMPType     string  `json:"icmpType"`
+	ICMPv6Type   string  `json:"icmpv6Type,omitempty"`
+}
+
+type Scope struct {
+	Type string            `json:"type"`
+	Data map[string]string `json:"data"`
+}
+
+type GetSecurityGroupQueryParameters struct {
+	CloudAccountId string
+	RegionId       string
+}
+
+type UpdateProtectionModeQueryParameters struct {
+	ProtectionMode string
+}
+
+func (service *Service) GetSecurityGroup(d9SecurityGroupID string) (*CloudSecurityGroupResponse, *http.Response, error) {
+	v := new(CloudSecurityGroupResponse)
+	relativeURL := fmt.Sprintf("%s/%s", awsSgResourcePath, d9SecurityGroupID)
+
+	resp, err := service.Client.NewRequestDo("GET", relativeURL, nil, nil, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// GetAllInRegion will return list of all the security groups in specific region
+func (service *Service) GetAllInRegion(d9CloudAccountID, awsRegionName string) (*[]CloudSecurityGroupResponse, *http.Response, error) {
+	if d9CloudAccountID == "" && awsRegionName == "" {
+		return nil, nil, fmt.Errorf("d9 cloud account ID and aws region name must be passed")
+	}
+
+	options := GetSecurityGroupQueryParameters{
+		CloudAccountId: d9CloudAccountID,
+		RegionId:       awsRegionName,
+	}
+
+	v := new([]CloudSecurityGroupResponse)
+	resp, err := service.Client.NewRequestDo("GET", awsSgResourcePath, options, nil, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// GetAll will return list of all the security groups in the whole regions
+func (service *Service) GetAll() (*[]CloudSecurityGroupResponse, *http.Response, error) {
+	v := new([]CloudSecurityGroupResponse)
+	resp, err := service.Client.NewRequestDo("GET", awsSgResourcePath, nil, nil, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+func (service *Service) Create(body CloudSecurityGroupRequest) (*CloudSecurityGroupResponse, *http.Response, error) {
+	v := new(CloudSecurityGroupResponse)
+	resp, err := service.Client.NewRequestDo("POST", awsSgResourcePath, nil, body, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// update aws security group, in order to update the field isProtected we should call the function UpdateProtectionMode
+func (service *Service) Update(d9SecurityGroupID string, body CloudSecurityGroupRequest) (*CloudSecurityGroupResponse, *http.Response, error) {
+	v := new(CloudSecurityGroupResponse)
+	relativeURL := fmt.Sprintf("%s/%s", awsSgResourcePath, d9SecurityGroupID)
+
+	resp, err := service.Client.NewRequestDo("PUT", relativeURL, nil, body, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// update protection mode is post api call, mode can be one of the following "FullManage","ReadOnly"
+func (service *Service) UpdateProtectionMode(d9SecurityGroupID, protectionMode string) (*CloudSecurityGroupResponse, *http.Response, error) {
+	if protectionMode != "FullManage" && protectionMode != "ReadOnly" {
+		return nil, nil, fmt.Errorf("protection mode can be FullManage or ReadOnly")
+	}
+
+	v := new(CloudSecurityGroupResponse)
+	relativeURL := fmt.Sprintf("%s/%s/%s", awsSgResourcePath, d9SecurityGroupID, awsSgResourceProtectionMode)
+	body := UpdateProtectionModeQueryParameters{
+		ProtectionMode: protectionMode,
+	}
+
+	resp, err := service.Client.NewRequestDo("POST", relativeURL, nil, body, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+// create and attach or update bound service (post api call)
+// There is a bug when we trying to update read only security group, get 500 (internal error) ticket link: https://dome9-security.atlassian.net/browse/DOME-12737
+func (service *Service) HandelBoundServices(d9SecurityGroupID, policyType string, boundService BoundServicesRequest) (*CloudSecurityGroupResponse, *http.Response, error) {
+	v := new(CloudSecurityGroupResponse)
+	relativeURL := fmt.Sprintf("%s/%s/%s/%s", awsSgResourcePath, d9SecurityGroupID, awsSgResourceServices, policyType)
+
+	resp, err := service.Client.NewRequestDo("POST", relativeURL, nil, boundService, v)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return v, resp, nil
+}
+
+func (service *Service) Delete(d9SecurityGroupID string) (*http.Response, error) {
+	relativeURL := fmt.Sprintf("%s/%s", awsSgResourcePath, d9SecurityGroupID)
+	resp, err := service.Client.NewRequestDo("DELETE", relativeURL, nil, nil, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
