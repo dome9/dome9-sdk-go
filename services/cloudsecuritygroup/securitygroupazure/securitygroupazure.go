61a38953e4eba45639c25969e100195cdab25aa9
Ayal <ayalg@checkpoint.com>
2019-12-10 18:10:15 +0200

    AWS security group fix

--- services/cloudsecuritygroup/securitygroupazure/securitygroupazure.go
+++ services/cloudsecuritygroup/securitygroupazure/securitygroupazure.go
@@ -9,34 +9,35 @@ const (
 	azureSgResourcePath = "AzureSecurityGroupPolicy"
 )
 
-type CloudSecurityGroupRequest struct {
+type AzureSecurityGroupRequest struct {
 	Name              string         `json:"name"`
-	Description       string         `json:"description"`
 	Region            string         `json:"region"`
-	IsTamperProtected bool           `json:"isTamperProtected"`
 	ResourceGroup     string         `json:"resourceGroup"`
 	CloudAccountID    string         `json:"cloudAccountId"`
+	Description       string         `json:"description"`
+	IsTamperProtected bool           `json:"isTamperProtected"`
+	Tags              []Tags         `json:"tags,omitempty"`
+	InboundServices   []BoundService `json:"inboundServices,omitempty"`
+	OutboundServices  []BoundService `json:"outboundServices,omitempty"`
+}
+
+type AzureSecurityGroupResponse struct {
+	ID                string         `json:"id"`
+	Name              string         `json:"name"`
+	Region            string         `json:"region"`
+	ResourceGroup     string         `json:"resourceGroup"`
+	CloudAccountID    string         `json:"cloudAccountId"`
+	Description       string         `json:"description"`
+	IsTamperProtected bool           `json:"isTamperProtected"`
 	Tags              []Tags         `json:"tags"`
 	InboundServices   []BoundService `json:"inboundServices"`
 	OutboundServices  []BoundService `json:"outboundServices"`
-}
 
-type CloudSecurityGroupResponse struct {
-	ID                      string         `json:"id"`
-	ExternalSecurityGroupID string         `json:"externalSecurityGroupId"`
-	AccountID               int            `json:"accountId"`
-	CloudAccountName        string         `json:"cloudAccountName"`
-	LastUpdatedByDome9      bool           `json:"lastUpdatedByDome9"`
-	Error                   Error          `json:"error"`
-	CloudAccountID          string         `json:"cloudAccountId"`
-	Name                    string         `json:"name"`
-	Description             string         `json:"description"`
-	Region                  string         `json:"region"`
-	ResourceGroup           string         `json:"resourceGroup"`
-	InboundServices         []BoundService `json:"inboundServices"`
-	OutboundServices        []BoundService `json:"outboundServices"`
-	IsTamperProtected       bool           `json:"isTamperProtected"`
-	Tags                    []Tags         `json:"tags"`
+	ExternalSecurityGroupID string `json:"externalSecurityGroupId"`
+	AccountID               int    `json:"accountId"`
+	CloudAccountName        string `json:"cloudAccountName"`
+	LastUpdatedByDome9      bool   `json:"lastUpdatedByDome9"`
+	Error                   Error  `json:"error"`
 }
 
 type Tags struct {
@@ -45,17 +46,17 @@ type Tags struct {
 }
 
 type Scope struct {
-	Type string            `json:"type"`
-	Data map[string]string `json:"data"`
+	Type string                 `json:"type"`
+	Data map[string]interface{} `json:"data"`
 }
 
 type BoundService struct {
-	Direction             string   `json:"direction"`
 	Name                  string   `json:"name"`
 	Description           string   `json:"description"`
 	Priority              int      `json:"priority"`
 	Access                string   `json:"access"`
 	Protocol              string   `json:"protocol"`
+	Direction             string   `json:"direction"`
 	SourcePortRanges      []string `json:"sourcePortRanges"`
 	SourceScopes          []Scope  `json:"sourceScopes"`
 	DestinationPortRanges []string `json:"destinationPortRanges"`
@@ -68,8 +69,8 @@ type Error struct {
 	ErrorMessage string `json:"errorMessage"`
 }
 
-func (service *Service) Get(id string) (*CloudSecurityGroupResponse, *http.Response, error) {
-	v := new(CloudSecurityGroupResponse)
+func (service *Service) Get(id string) (*AzureSecurityGroupResponse, *http.Response, error) {
+	v := new(AzureSecurityGroupResponse)
 	relativeURL := fmt.Sprintf("%s/%s", azureSgResourcePath, id)
 	resp, err := service.Client.NewRequestDo("GET", relativeURL, nil, nil, v)
 	if err != nil {
@@ -79,8 +80,8 @@ func (service *Service) Get(id string) (*CloudSecurityGroupResponse, *http.Respo
 	return v, resp, nil
 }
 
-func (service *Service) GetAll() (*[]CloudSecurityGroupResponse, *http.Response, error) {
-	v := new([]CloudSecurityGroupResponse)
+func (service *Service) GetAll() (*[]AzureSecurityGroupResponse, *http.Response, error) {
+	v := new([]AzureSecurityGroupResponse)
 	resp, err := service.Client.NewRequestDo("GET", azureSgResourcePath, nil, nil, v)
 	if err != nil {
 		return nil, nil, err
@@ -89,8 +90,8 @@ func (service *Service) GetAll() (*[]CloudSecurityGroupResponse, *http.Response,
 	return v, resp, nil
 }
 
-func (service *Service) Create(body CloudSecurityGroupRequest) (*CloudSecurityGroupResponse, *http.Response, error) {
-	v := new(CloudSecurityGroupResponse)
+func (service *Service) Create(body AzureSecurityGroupRequest) (*AzureSecurityGroupResponse, *http.Response, error) {
+	v := new(AzureSecurityGroupResponse)
 	resp, err := service.Client.NewRequestDo("POST", azureSgResourcePath, nil, body, v)
 	if err != nil {
 		return nil, nil, err
@@ -109,8 +110,8 @@ func (service *Service) Delete(id string) (*http.Response, error) {
 	return resp, nil
 }
 
-func (service *Service) Update(id string, body CloudSecurityGroupRequest) (*CloudSecurityGroupResponse, *http.Response, error) {
-	v := new(CloudSecurityGroupResponse)
+func (service *Service) Update(id string, body AzureSecurityGroupRequest) (*AzureSecurityGroupResponse, *http.Response, error) {
+	v := new(AzureSecurityGroupResponse)
 	relativeURL := fmt.Sprintf("%s/%s", azureSgResourcePath, id)
 	resp, err := service.Client.NewRequestDo("PUT", relativeURL, nil, body, v)
 	if err != nil {
