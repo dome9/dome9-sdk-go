ae8cbb1629749e670902d26a67a8a48922e6e4a2
Dolev <dolevh@checkpoint.com>
2021-11-22 13:40:24 +0200

    PLAT-175-onboard -Alibaba-Provider

--- services/cloudaccounts/cloudaccounts.go
+++ services/cloudaccounts/cloudaccounts.go
@@ -4,7 +4,8 @@ const (
 	RESTfulPathAWS   = "cloudaccounts"
 	RESTfulPathAzure = "AzureCloudAccount"
 	RESTfulPathGCP   = "GoogleCloudAccount"
-	RESTfulPathK8S   = "KubernetesAccount"
+	RESTfulPathK8S     = "KubernetesAccount"
+	RESTfulPathAlibaba = "AlibabaCloudAccount"
 )
 
 // AWS service paths
@@ -48,6 +49,13 @@ const (
 	RESTfulPathK8SAdmissionControl = "admission-control"
 	//image-assurance
 	RESTfulPathK8SImageAssurance = "vulnerabilityAssessment"
+)
+
+// Alibaba service paths
+const (
+	RESTfulServicePathAlibabaName               = "AccountName"
+	RESTfulServicePathAlibabaOrganizationalUnit = "organizationalUnit"
+	RESTfulServicePathAlibabaCredentials        = "Credentials"
 )
 
 type QueryParameters struct {
