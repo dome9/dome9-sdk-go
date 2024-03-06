fb7cc9e3e993b2014ec7893ba4b55c11146c9142
Omar <omark@checkpoint.com>
2024-03-06 14:13:10 +0200

    update go structs for get cloud account id response

--- services/awp_aws_onboarding/awp_aws_onboarding.go
+++ services/awp_aws_onboarding/awp_aws_onboarding.go
@@ -2,6 +2,7 @@ package awp_aws_onboarding
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -23,7 +24,51 @@ type AgentlessAwsTerraformOnboardingDataResponse struct {
 }
 
 type CloudAccountResponse struct {
-	ID string `json:"id"`
+	ID                     string      `json:"id"`
+	Vendor                 string      `json:"vendor"`
+	Name                   string      `json:"name"`
+	ExternalAccountNumber  string      `json:"externalAccountNumber"`
+	Error                  interface{} `json:"error"`
+	IsFetchingSuspended    bool        `json:"isFetchingSuspended"`
+	CreationDate           string      `json:"creationDate"`
+	Credentials            Credentials `json:"credentials"`
+	IamSafe                interface{} `json:"iamSafe"`
+	NetSec                 NetSec      `json:"netSec"`
+	Magellan               bool        `json:"magellan"`
+	FullProtection         bool        `json:"fullProtection"`
+	AllowReadOnly          bool        `json:"allowReadOnly"`
+	OrganizationId         string      `json:"organizationId"`
+	OrganizationalUnitId   interface{} `json:"organizationalUnitId"`
+	OrganizationalUnitPath string      `json:"organizationalUnitPath"`
+	OrganizationalUnitName string      `json:"organizationalUnitName"`
+	LambdaScanner          bool        `json:"lambdaScanner"`
+	Serverless             Serverless  `json:"serverless"`
+	OnboardingMode         string      `json:"onboardingMode"`
+}
+
+type Credentials struct {
+	Apikey     interface{} `json:"apikey"`
+	Arn        string      `json:"arn"`
+	Secret     interface{} `json:"secret"`
+	IamUser    interface{} `json:"iamUser"`
+	Type       string      `json:"type"`
+	IsReadOnly bool        `json:"isReadOnly"`
+}
+
+type NetSec struct {
+	Regions []Region `json:"regions"`
+}
+
+type Region struct {
+	Region           string `json:"region"`
+	Name             string `json:"name"`
+	Hidden           bool   `json:"hidden"`
+	NewGroupBehavior string `json:"newGroupBehavior"`
+}
+
+type Serverless struct {
+	CodeAnalyzerEnabled           bool `json:"codeAnalyzerEnabled"`
+	CodeDependencyAnalyzerEnabled bool `json:"codeDependencyAnalyzerEnabled"`
 }
 
 func (service *Service) Get() (*AgentlessAwsTerraformOnboardingDataResponse, *http.Response, error) {
@@ -39,6 +84,7 @@ func (service *Service) Get() (*AgentlessAwsTerraformOnboardingDataResponse, *ht
 func (service *Service) GetCloudAccountId(externalAccountId string) (string, *http.Response, error) {
 	path := fmt.Sprintf("%s%s", cloudAccountsPath, externalAccountId)
 	respData := new(CloudAccountResponse)
+	log.Printf("[DEBUG] GetCloudAccountId Path: %s", path)
 	resp, err := service.Client.NewRequestDo("GET", path, nil, nil, respData)
 	if err != nil {
 		return "", nil, err
