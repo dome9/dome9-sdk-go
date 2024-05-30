801924904d2122139767a8609f6dc3d629b3cb2b
DolevH <dolevh@checkpoint.com>
2024-05-30 16:45:57 +0300

    line

--- services/cloudaccounts/aws_org/aws_org.go
+++ services/cloudaccounts/aws_org/aws_org.go
@@ -4,7 +4,6 @@ import (
 	_ "encoding/json"
 )
 
-// CloudCredentialsType enum
 type CloudCredentialsType string
 
 const (
@@ -13,46 +12,26 @@ const (
 )
 
 type OnboardingPermissionRequest struct {
-	// CloudGuard role arn from AWS.
-	// AWS China accounts supported only in CloudGuard China DC.
-	RoleArn string `json:"roleArn" validate:"required,roleArn"`
-
-	// Also known as ExternalId from management-stack API.
-	// this value should be the one that returns from: management-stack API.
-	Secret string `json:"secret" validate:"required,secret"`
-
-	// Should be null.
-	// Needed only for 'UserBased' Type.
-	ApiKey *string `json:"apiKey,omitempty"`
-
-	// Default value is 'RoleBased'.
-	// 'UserBased' is not supported.
-	Type CloudCredentialsType `json:"type" validate:"required,oneof=UserBased RoleBased"`
+	RoleArn string               `json:"roleArn" validate:"required,roleArn"`
+	Secret  string               `json:"secret" validate:"required,secret"`
+	ApiKey  *string              `json:"apiKey,omitempty"`
+	Type    CloudCredentialsType `json:"type" validate:"required,oneof=UserBased RoleBased"`
 }
 
 type ValidateStackSetArnRequest struct {
 	OnboardingPermissionRequest
-
-	// The created StackSet ARN.
 	StackSetArn string `json:"stackSetArn" validate:"required,stackSetArn"`
 }
 
 type OnboardingRequest struct {
 	ValidateStackSetArnRequest
-
-	// Not required.
 	AwsOrganizationName *string `json:"awsOrganizationName,omitempty"`
-
-	// Required. Default is false, it's for future use.
-	EnableStackModify bool `json:"enableStackModify" validate:"required"`
+	EnableStackModify   bool    `json:"enableStackModify" validate:"required"`
 }
 
 type OnboardingUpdateRequest struct {
-	// AWS organization name.
 	AwsOrganizationName *string `json:"awsOrganizationName,omitempty"`
-
-	// Required. Default is false, it's for future use.
-	EnableStackModify bool `json:"enableStackModify"`
+	EnableStackModify   bool    `json:"enableStackModify"`
 }
 
 type UpdateConfigurationRequest struct {
@@ -81,6 +60,5 @@ const (
 )
 
 type UpdateStackSetArnRequest struct {
-	// The created StackSet ARN.
 	StackSetArn string `json:"stackSetArn" validate:"required,stackSetArn"`
 }
